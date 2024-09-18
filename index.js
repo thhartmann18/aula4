49fdf7c6a2733b94d4eb2002758ac757a3df772f
Th <thiago.r.hartmann@gmail.com>
2024-09-18 11:39:53 -0300

    exercicio aula 4

---
+++ index.js
@@ -0,0 +1,78 @@
+const primeiroValor = 10
+
+const segundoValor = 20
+
+const resultado = primeiroValor + segundoValor + 4
+
+console.log(resultado) 
+
+const primeirovalorr = 10
+
+const segundovalorr = 20
+
+const resultadoo = primeirovalorr + segundovalorr 
+
+console.log(resultadoo)
+
+
+const primeirovaloorr =3
+
+const segundovaloorr =5
+
+const resultadooo = primeirovaloorr*segundovaloorr /2
+
+console.log(resultadooo)
+
+
+const valorp =5
+
+const valors =4
+
+const resul=valorp-valors-1
+
+console.log(resul)
+
+
+const val =234
+
+const vall =5
+
+const resu = val / vall
+
+console.log(resu)
+
+const novava=1
+
+const m=2
+
+console.log(novava===m)
+console.log(novava!==m)
+console.log(novava>-m)
+console.log(novava<-m)
+
+const a=true 
+const b=false
+const c=true
+
+console.log(a&&b)
+console.log(b&&c)
+console.log(c&&b)
+console.log(a&&b&&c)
+
+const aa=true
+const bb=false 
+const cc=true 
+
+console.log(aa||bb)
+console.log(bb||cc)
+console.log(aa||cc)
+console.log(aa||bb||cc)
+
+let nome =prompt("qual e seu nome?")
+let born = Number(prompt("qual e sua data de nacimento?"))
+let ano = Number (prompt ("qual e seu ano atual"))
+
+console.log ("seu nome é",nome)
+console.log ("voce tem",ano-born,"anos de idade")
+console.log ("é maior de idade",ano-born >=18)
+console.log ("em 2050 voce tera",2050 -born,"anos de idade")
